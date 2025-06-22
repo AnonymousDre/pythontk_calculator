a598384e8efd90cd2a0c335813160cf56222c971
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2025-06-22 23:13:35 +0800

    Added a plus operator

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ window.title("GUI Calculator")
 
 icon = PhotoImage(file="calculator_image.png")
 window.iconphoto(False, icon)
+window.config(bg="#33324b")
+
+plus = Button(window, text="+", font=("Arial", 24), bg="#22222c", fg="white")
+plus.place(x=450, y=250)
 window.mainloop()
