5b346b485f75f16cbf65d3b3c55715639ca9a45c
Andre Quizon <135997258+AnonymousDre@users.noreply.github.com>
2025-06-23 22:35:28 +0800

    added a create_button function

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ icon = PhotoImage(file="calculator_image.png")
 window.iconphoto(False, icon)
 window.config(bg="#33324b")
 
-plus = Button(window, text = "+", font = ("Arial", 24), bg = "#22222c", fg = "white")
-minus = Button(window, text = "-", font = ("Arial", 24), bg = "#22222c", fg = "white")
-multi = Button(window, text = "x", font = ("Arial", 24), bg = "#22222c", fg = "white")
-equal = Button(window, text = "=", font = ("Arial", 24), bg = "#22222c", fg = "white")
+def create_button(window, text):
+    return Button(window, text=text, font=("Arial", 24), bg="#22222c", fg="white")
+
+plus = create_button(window, "+")
+minus = create_button(window, "+")
+multi = create_button(window, "x")
+equal = create_button(window, "=")
+
+zero = create_button(window, "0")
+one = create_button(window, "1")
+two = create_button(window, "2")
+three = create_button(window, "3")
+
 plus.place(relx = 0.9, rely = 0.35, anchor=W, width=50, height=50)
 minus.place(relx = 0.9, rely = 0.425, anchor = W, width = 50, height = 50)
 multi.place(relx = 0.9, rely = 0.50, anchor= W, width = 50, height = 50)
